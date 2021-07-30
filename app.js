771c307464cdf08a85d4a8ee608faff418ac5898
Benedict <jokanolaadedipupo@gmail.com>
2021-07-30 08:11:38 +0100

    server

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.use(express.json())
 const port = process.env.PORT || 8080;
 
 
-app.get('/', ()=>{
+app.get('/', (req,res)=>{
     return res.send('Hello World!')
 })
 
-app.listen('/', ()=>{
+app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
