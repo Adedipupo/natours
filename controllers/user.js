67d92b82323e7d46d7a9b270e1f30c16f259da6d
Benedict <jokanolaadedipupo@gmail.com>
2021-07-30 10:08:54 +0100

    add

--- controllers/user.js
+++ controllers/user.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 const users = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/users.json`));
 
 
-const getAllUsers = (req,res)=>{
+exports.getAllUsers = (req,res)=>{
     return res.status(200).json({msg: 'Success',results: users.length, data: users})
 }
-
-module.exports = {getAllUsers}
