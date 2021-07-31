42f9f4da02ea3331b324f4a6fc41c783a08c0397
Benedict <jokanolaadedipupo@gmail.com>
2021-07-31 09:03:13 +0100

    server working

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const indexRouter = require('./routes/index');
+const { connectDB } = require('./config/db');
 
 dotenv.config();
-
+connectDB();
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json())
@@ -16,9 +17,9 @@ const port = process.env.PORT || 8080;
 app.use('/api/v1', indexRouter);
 
 app.get('/', (_req, res) => {
-    res.redirect('/api/v1');
-  });
+  res.redirect('/api/v1');
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
