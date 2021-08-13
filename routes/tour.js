91e5ba24ca08b0e98e6e8c57ab33e83addaad3d7
Benedict <jokanolaadedipupo@gmail.com>
2021-08-13 19:35:59 +0100

    routes setup

--- routes/tour.js
+++ routes/tour.js
@@ -1,9 +1,12 @@
-const { getAllTours, createTour } = require("../controllers/tour");
+const { getAllTours, createTour, deleteTours, getOneTour } = require("../controllers/tour");
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', getAllTours)
-router.post('/create', createTour)
+router.get('/', getAllTours);
+router.get('/:id', getOneTour);
+router.post('/create', createTour);
+router.delete('/:id', deleteTour);
+
 
 module.exports =router;
