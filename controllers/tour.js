d80c25ba87fb33b4a1471364f79e48c481fb1d04
Benedict <jokanolaadedipupo@gmail.com>
2021-08-13 19:26:24 +0100

    get all tours

--- controllers/tour.js
+++ controllers/tour.js
@@ -3,12 +3,25 @@ const { TourModel } = require('../models/tourModel');
 
 
 exports.createTour = async(req,res)=>{
-    const {name,rating,price} = req.body;
+    try {
+        const {name,rating,price} = req.body;
 
     const tour = await TourModel.create(req.body);
+    if(tour){
+        return res.status(201).json({message: 'Success', data: tour})
+    }        
+    } catch (error) {
+     return res.status(404).json({message: error.message})   
+    }
+
 }
 
-exports.getAllTours = (req,res)=>{
-    return res.status(200).json({msg: 'Success',results: tours.length, data: tours})
+exports.getAllTours = async(req,res)=>{
+    try {
+        const tours = await TourModel.find();
+        return res.status(200).json({msg: 'Success',results: tours.length, data: tours})
+    } catch (error) {
+        return res.status(404).json({message: error.message})   
+    }
 }
 
--- routes/tour.js
+++ routes/tour.js
@@ -1,8 +1,9 @@
-const { getAllTours } = require("../controllers/tour");
+const { getAllTours, createTour } = require("../controllers/tour");
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', getAllTours)
+router.post('/create', createTour)
 
 module.exports =router;
