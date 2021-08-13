6ebcf58335b0c3e9dbce6b369d1c6cacf11d4d30
Benedict <jokanolaadedipupo@gmail.com>
2021-08-13 10:22:12 +0100

    refactoring

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.json())
 
 const port = process.env.PORT || 8080;
 
+app.use((req,res,next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
 app.use('/api/v1', indexRouter);
 
 app.get('/', (_req, res) => {
