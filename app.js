5fdd065f2e109f5cc68e07b83370e307913447f8
Benedict <jokanolaadedipupo@gmail.com>
2021-08-16 09:48:00 +0100

    error middleware

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use('/api/v1', indexRouter);
-app.all('*',(req,res,next)=>{
-  res.status(404).json({status:'Failed' ,message: `Can't find the ${req.originalUrl} on the server`});
-  next()
+
+app.all('*', (req, res, next) => {
   const err = new Error(`Can't find the ${req.originalUrl} on the server`)
   err.status = 'fail';
   err.statusCode = 404;
+  next(err)
 })
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
