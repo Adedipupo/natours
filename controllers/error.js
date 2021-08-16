b616502aacbc046875b42fcb3e302f75a3b0cf1f
Benedict <jokanolaadedipupo@gmail.com>
2021-08-16 21:10:33 +0100

    error

--- controllers/error.js
+++ controllers/error.js
@@ -2,8 +2,17 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
   
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    })
+    if (process.env.NODE_ENV === 'development') {
+        res.status(err.statusCode).json({
+          status: err.status,
+          error: err,
+          message: err.message,
+          stack: err.stack
+        })
+      }else if(process.env.NODE_ENV === 'production'){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+          })
+      }
   }
