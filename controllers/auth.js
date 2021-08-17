dcd4536cc711a664077690c195acc86815fcd9b3
Benedict <jokanolaadedipupo@gmail.com>
2021-08-17 11:00:18 +0100

    user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,12 @@ const catchAsync = require("../utils/catchAsync");
 
 
 exports.register = catchAsync(async(req,res) => {
-    const user = await UserModel.create(req.body);
+    const user = await UserModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
     return res.status(201).json({
         status: 'success',
         data: user
