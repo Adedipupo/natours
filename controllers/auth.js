9b36750b3bf8375d2fd7383aa6807d97c26fb1c2
Benedict <jokanolaadedipupo@gmail.com>
2021-08-20 09:17:53 +0100

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { UserModel } = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 
 exports.register = catchAsync(async(req,res) => {
@@ -12,6 +13,22 @@ exports.register = catchAsync(async(req,res) => {
     });
 
     const token = jwt.sign({id: user._id},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRES_IN})
+    return res.status(201).json({
+        status: 'success',
+        token,
+        data: user
+    })
+})
+
+exports.login = catchAsync(async(req,res) => {
+    const {email,password} = req.body;
+
+    if(!email || !password){
+      return   next(new AppError('Please provide email and password!',400))
+    }
+
+    const user = await UserModel.findOne({email}).select('+password');
+    const token = '';
     return res.status(201).json({
         status: 'success',
         token,
