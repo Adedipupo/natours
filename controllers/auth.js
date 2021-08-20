7fa9550c23cdf9e9e9f2d8117812184e5142720c
Benedict <jokanolaadedipupo@gmail.com>
2021-08-20 10:50:56 +0100

    protected routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,7 +38,39 @@ exports.login = catchAsync(async (req, res) => {
     const token = generateToken(user._id);
     return res.status(201).json({
         status: 'success',
-        token,
-        data: user
+        token
     })
 })
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+  
+    if (!token) {
+      return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+  
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(new AppError('The user belonging to this token does no longer exist.', 401));
+    }
+  
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next(new AppError('User recently changed password! Please log in again.', 401));
+    }
+  
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    res.locals.user = currentUser;
+    next();
+  });
