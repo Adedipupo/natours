afdd51600834b812f3461f7eb724d2780902a322
Benedict <jokanolaadedipupo@gmail.com>
2021-08-20 09:07:27 +0100

    signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,6 +14,7 @@ exports.register = catchAsync(async(req,res) => {
     const token = jwt.sign({id: user._id},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRES_IN})
     return res.status(201).json({
         status: 'success',
+        token,
         data: user
     })
 })
