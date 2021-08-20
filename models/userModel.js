1b611c28cd930c36918a5ea96da407133470370b
Benedict <jokanolaadedipupo@gmail.com>
2021-08-20 08:06:43 +0100

    password

--- models/userModel.js
+++ models/userModel.js
@@ -55,7 +55,7 @@ userSchema.pre('save', async function(next) {
   // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
 
-  // Hash the password with cost of 12
+  // Hash the password with cost of 123
   this.password = await bcrypt.hash(this.password, 12);
 
   // Delete passwordConfirm field
